d7b3ae644ad57ab6723abbc3593415b802a60f27
imgoodman <jan8611@163.com>
2017-06-15 20:24:28 +0800

    data is loaded

--- ch3/mnist-demo.py
+++ ch3/mnist-demo.py
@@ -2,4 +2,4 @@ from tensorflow.examples.tutorials.mnist import input_data
 
 data_path="/usr/bigdata/data/mnist_data/"
 
-mnist=input_data.read_data_sets(data_path, one_hot=True)
+#mnist=input_data.read_data_sets(data_path, one_hot=True)
